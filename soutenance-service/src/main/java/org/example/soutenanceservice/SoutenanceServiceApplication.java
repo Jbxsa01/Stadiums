8096aa46f21b584c57a0d38965d03fca454ac451
nouhailaelkhaoudi2-dev <nouhaila.elkhaoudi2@gmail.com>
2025-11-24 20:46:28 +0100

    initialisation

---
+++ soutenance-service/src/main/java/org/example/soutenanceservice/SoutenanceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.soutenanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutenanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoutenanceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/doctorat/DoctoratApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doctorat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctoratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
