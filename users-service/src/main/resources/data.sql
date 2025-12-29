379f5c2345491ace008d8eec62f9b67f7d0454f0
maryame <maryamaitmanssour@gmail.com>
2025-12-29 21:50:01 +0100

    step

---
+++ users-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (nom, prenom, email, password, role) VALUES
+                                                           ('Benali', 'Ahmed', 'ahmed.benali@universite.dz', 'password123', 'ADMIN'),
+                                                           ('Hamdi', 'Fatima', 'fatima.hamdi@universite.dz', 'password123', 'DOCTORANT'),
+                                                           ('Meziane', 'Karim', 'karim.meziane@universite.dz', 'password123', 'DOCTORANT'),
+                                                           ('Saidi', 'Amina', 'amina.saidi@universite.dz', 'password123', 'DIRECTEUR'),
+                                                           ('Boumediene', 'Rachid', 'rachid.boumediene@universite.dz', 'password123', 'DIRECTEUR');
