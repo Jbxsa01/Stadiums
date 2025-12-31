d110ee6416b91df8d3d9fb84a66c1ddd791007c2
jbxsa01 <bjane.asmaa@gmail.com>
2025-12-31 07:59:59 +0100

    update v3

---
+++ reservation-service/src/main/java/org/example/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,14 @@
+
+package org.example.reservationservice;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ReservationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+}
