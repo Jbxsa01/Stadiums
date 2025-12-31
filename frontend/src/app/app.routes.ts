b0729781c9783e703492f4ab457af3dab0b4b50b
jbxsa01 <bjane.asmaa@gmail.com>
2025-12-31 05:39:31 +0100

    last updat

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inscription',
+    loadChildren: () =>
+      import('./inscription/inscription.module').then((m) => m.InscriptionModule),
+  },
+  { path: '', redirectTo: 'inscription', pathMatch: 'full' },
+];
---
+++ frontend/src/app/inscription/inscription-dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription-dashboard',
+  templateUrl: './inscription-dashboard.component.html',
+  styleUrls: ['./inscription-dashboard.component.scss']
+})
+export class InscriptionDashboardComponent {}
