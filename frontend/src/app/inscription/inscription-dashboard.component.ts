ce356544c5b86e084bef6815245ebb749c5d18ee
maryame <maryamaitmanssour@gmail.com>
2025-12-31 05:51:58 +0100

    step

--- frontend/src/app/inscription/inscription-dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-inscription-dashboard',
-  templateUrl: './inscription-dashboard.component.html',
-  styleUrls: ['./inscription-dashboard.component.scss']
-})
-export class InscriptionDashboardComponent {}
--- frontend/src/app/inscription/workflow.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-workflow',
-  templateUrl: './workflow.component.html',
-  styleUrls: ['./workflow.component.scss']
-})
-export class WorkflowComponent {}
